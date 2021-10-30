9322921d28a68e04d3dff1b3484e25f4cea41f1a
Francesco Didio <37405281+Fra3005@users.noreply.github.com>
2021-10-30 17:46:26 +0200

    start display fix

--- src/components/weather.js
+++ src/components/weather.js
@@ -1,67 +1,81 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import Autocomplete from "@mui/material/Autocomplete";
-import TextField from '@mui/material/TextField';
+import TextField from "@mui/material/TextField";
+import { Button, Card, CardContent, Grid, Typography } from "@mui/material";
 
 const url = {
-  base: "https://api.openweathermap.org/data/2.5/weather?q=London&appid=",
+  base: "https://api.openweathermap.org/data/2.5/weather?q=",
   apiId: "2a0380a6c0cc6fa2a7b4234aa352863a",
 };
 
-const citt = [{
-  name: "Bari"
-}]
-
 export default function Weather() {
-  const [api, setApi] = useState([]);
-  const [citta, setCitta] = useState([]);
+  const [api, setApi] = useState();
+  const [citta, setCitta] = useState("");
 
-  const getCitta = async () => {
+  const getWeather = (citta) => {
     try {
-      const response = await axios.get(
-        "https://raw.githubusercontent.com/matteocontrini/comuni-json/master/comuni.json"
-      );
-      setCitta(response.data.slice(0,99));
-      
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
-  const getWeather = async () => {
-    try {
-      const response = await axios.get(url.base + `${url.apiId}`);
-      setApi(JSON.stringify(response));
+      fetch(url.base + `${citta}` + "&appid=" + `${url.apiId}`)
+        .then((res) => res.json())
+        .then((result) => {
+          setApi(result);
+        });
     } catch (e) {
       console.log(e);
     }
   };
 
   useEffect(() => {
-    getWeather().then(() => {
-      console.log(api);
-    });
-    getCitta().then(() => {
-      console.log(citta);
-    });
-  }, []);
+    console.log(api);
+  }, [api]);
 
+  const converter = (temp) => {
+    temp = parseFloat(temp);
+    return temp - 273.15;
+  };
 
-  useEffect(()=>{
-
-      console.log("Citta", citta)
-  }, [citta])
   return (
-    <div>
-      <Autocomplete
-        
-        id="combo-box-demo"
-        options={citta}
-        sx={{ width: 300 }}
-        renderInput={(params) => <TextField {...params} label="Selezionare una città" />}
-      />
+    <div className="app">
+      <Grid container>
+        <Grid item xs={12}></Grid>
+        <Grid item xs={5}></Grid>
+        <Grid item xs={6}>
+          <TextField
+            id="outlined-basic"
+            label="Outlined"
+            variant="outlined"
+            value={citta}
+            onChange={(e) => {
+              setCitta(e.target.value);
+            }}
+          />
+          <Button
+            sx={{ height: "100%", marginLeft: "10px" }}
+            variant="contained"
+            onClick={() => {
+              getWeather(citta);
+            }}
+          >
+            Trova
+          </Button>
+        </Grid>
+        <Grid item xs={1}></Grid>
+        <Grid item xs={3}></Grid>
+        <Grid item xs={6}>
+          {api != undefined ? (
+            <Card>
+              <CardContent>
+                {" "}
+                <Typography>
+                  LOCATION: {api.name}, {api.sys.country}
+                </Typography>
+                <Typography>
+                  TEMPERATURA: {Math.round(converter(api.main.temp))}°C
+                </Typography>
+              </CardContent>
+            </Card>
+          ) : null}
+        </Grid>
+      </Grid>
     </div>
   );
 }
-
-
